ec859c5cfb1a223b3ce7f0550e455fba601e286f
sethrimer3 <sethrimer3@gmail.com>
2026-05-31 23:41:57 -0600

    asd

--- run-desktop.bat
+++ run-desktop.bat
@@ -2,6 +2,13 @@
 setlocal
 pushd "%~dp0"
 
+if /i not "%~1"=="--hidden" (
+  powershell -NoProfile -WindowStyle Hidden -Command "Start-Process -FilePath 'cmd.exe' -ArgumentList @('/c', '\"%~f0\" --hidden') -WorkingDirectory '%~dp0' -WindowStyle Hidden"
+  timeout /t 1 /nobreak >nul
+  popd
+  exit /b 0
+)
+
 if not exist package.json (
   echo ERROR: package.json was not found in this folder.
   goto error
